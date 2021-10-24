f007d0b65f1d54f545ac617f89f65eb89da68bf2
rubencanon <31599115+rubencanon@users.noreply.github.com>
2021-10-24 16:22:03 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './components/signin/signin.component';
 import { RegisterComponent } from './components/register/register.component';
+import { MonitorChartComponent } from './components/monitor-chart/monitor-chart.component';
+
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/' },
   { path: 'signin', component: SigninComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'monitor', component: MonitorChartComponent },
+
 ];
 
 @NgModule({
